7bb9853ab24f8bc67e0afdf192034e88da5c9e84
BT <hello@nolim1t.co>
2020-08-09 13:44:02 +0000

    change RSA1024 to ED25519-V3

--- src/torcontrol.cpp
+++ src/torcontrol.cpp
@@ -533,7 +533,7 @@ void TorController::auth_cb(TorControlConnection& _conn, const TorControlReply& 
 
         // Finally - now create the service
         if (private_key.empty()) // No private key, generate one
-            private_key = "NEW:RSA1024"; // Explicitly request RSA1024 - see issue #9214
+            private_key = "NEW:ED25519-V3"; // Explicitly request RSA1024 - see issue #9214. Lets try "ED25519-V3" tor
         // Request onion service, redirect port.
         // Note that the 'virtual' port is always the default port to avoid decloaking nodes using other ports.
         _conn.Command(strprintf("ADD_ONION %s Port=%i,127.0.0.1:%i", private_key, Params().GetDefaultPort(), GetListenPort()),
